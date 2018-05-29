6453bc0df67169e0091c254d7033a0ba1155f3c7
jssd <1624022009@qq.com>
2018-05-29 18:26:25 +0800

    加入多线程
    服务器加入多线程,  可以使多个客户端连接

--- MyChatRoom/src/ChatRoom/javaTest7/ChatSever.java
+++ MyChatRoom/src/ChatRoom/javaTest7/ChatSever.java
@@ -5,13 +5,16 @@ import java.io.*;
 
 public class ChatSever {
 
-	public static void main(String[] args) {
-		ServerSocket ss = null; // 服务端套接字
-		BufferedReader buffInt = null; // 取得客户端输入流
-		Socket s = null; // 客户端套接字
-		String str = null; // 接受客户端发来的消息
+	ServerSocket ss = null; // 服务端套接字
+	boolean status = false; // 服务器初始化操作循环标志
 
-		boolean status = false; // 服务器初始化操作循环标志
+	public static void main(String[] args) {
+		new ChatSever().start();
+	}
+
+	
+	public void start() {
+		
 		try {
 			ss = new ServerSocket(8888);
 			status = true;
@@ -27,35 +30,71 @@ public class ChatSever {
 				e1.printStackTrace();
 			}
 		}
-
-		while (status) {
-			boolean bConnected = false; // 客户端连接成功操作循环标志
+		
+		try {
+			while (status) {
+				Socket s = ss.accept(); // 客户端连接
+				new Thread(new Clint(s)).start();;
+			}
+		} catch (Exception e) {
+				System.out.println("Clint disconnected!");
+		} finally {
 			try {
-				s = ss.accept(); // 客户端连接
-				System.out.println("a clint connection! "); // 已经连接注释
+				ss.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.out.println("Sever is closed");
+			}
+		}
+	}
+	
+	
+	private class Clint implements Runnable {
+		
+		private BufferedReader buffInt = null; // 取得客户端输入流
+		private Socket s = null; // 客户端套接字
+		private String str = null; // 接受客户端发来的消息
+		private boolean bConnected = false;
+		
+		public Clint(Socket s) {
+			this.s = s;
+			System.out.println("a clint connection! "); // 已经连接注释
+			bConnected = true;
+			try {
 				buffInt = new BufferedReader(new InputStreamReader( // 初始化客户端输入流
 						s.getInputStream()));
-				bConnected = true;
-				while (bConnected) { // 连接成功操作循环
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		@Override
+		public void run() {
+			while (bConnected) { // 连接成功操作循环
+				try {
 					str = buffInt.readLine();
 					if (str != null) {
 						System.out.println(str); //将客户端发送信息打印到客户端控制台
 					} else {
 						bConnected = false;
 					}
+				} catch (IOException e) {
+					e.printStackTrace();
 				}
-			} catch (Exception e) {
-				System.out.println("Clint closed");
-			} finally {
-				try {
-					if (buffInt != null)
-						buffInt.close();
-					if (s != null)
-						s.close();
-					System.out.println("客户端关闭"); // 客户端关闭操作注释
-				} catch (IOException e1) {
-					e1.printStackTrace();
-				}
+			}
+			
+			try {
+				buffInt.close();
+				System.out.println("输入流关闭");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			try {
+				s.close();
+				System.out.println("客户端连接关闭");
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 		}
 	}
